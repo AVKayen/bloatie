7b75e8fd4b6084079d6451db95ccc3e37885451d
Aveerator <aganitnt@gmail.com>
2023-04-16 20:11:02 +0200

    new file:   .gitignore 	new file:   Cargo.lock 	new file:   Cargo.toml 	new file:   src/main.rs

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+use std::env;
+use std::path::Path;
+use std::fs::{File, remove_file};
+use std::io::Result;
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() >= 4  {
+        let pathstr = &args[2];
+            let name = &args[3];
+            let path = Path::new(pathstr);
+            if path.is_dir() {
+                if args[1] == "bloat" {
+                    bloat_folder(path, &name)?;
+                } else if args[1] == "debloat" {
+                    debloat_folder(path, &name)?;
+                } else {
+                    eprintln!("Nieprawidłowa metoda: {}. Poprawne to 'bloat' i 'debloat'.", args[1]);
+                }
+            
+        } 
+            Ok(())
+        }
+     else {
+        println!("Użycie:");
+        println!("  $ bloatie bloat . hehe.bloater     # tworzy plik \"haha.bloater\" w każdej podścieżce ścieżki \".\"");
+        println!("  $ bloatie bloat . hehe.bloater     # usuwa plik \"haha.bloater\" z każdej podścieżki ścieżki \".\"");
+        Ok(())
+    }
+}
+
+fn bloat_folder(path: &Path, name: &str) -> Result<()> {
+    println!("Creating {} in {:?}", name, path);
+    let file = Path::new(path).join(name);
+    File::create(file)?;
+    for entry in path.read_dir()? {
+        let dir = entry?.path();
+        if dir.is_dir() {
+            bloat_folder(dir.as_path(), name)?;
+        }
+    }
+    Ok(())
+}
+
+fn debloat_folder(path: &Path, name: &str) -> Result<()> {
+    println!("Removed {} from {:?}", name, path);
+    let file = Path::new(path).join(name);
+    remove_file(file)?;
+    for entry in path.read_dir()? {
+        let dir = entry?.path();
+        if dir.is_dir() {
+            debloat_folder(dir.as_path(), name)?;
+        }
+    }
+    Ok(())
+}
